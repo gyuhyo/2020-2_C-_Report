20a51098d4e73d7a8f25aff6b06c1cf7f5c4847e
ParkGyuHyo <55084952+gyuhyo@users.noreply.github.com>
2020-09-12 21:10:29 +0900

    Add files via upload

---
+++ Report_1_Week/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("이름 : 박규효");
+            Console.WriteLine("학번 : 201987062");
+        }
+    }
+}
