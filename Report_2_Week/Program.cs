931c9c42305bf5287e744f505eefef14d489e214
ParkGyuHyo <55084952+gyuhyo@users.noreply.github.com>
2020-09-20 09:13:26 +0900

    Add files via upload

---
+++ Report_2_Week/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 25;
+            int num2 = 10;
+
+            Console.WriteLine("정수1 : " + num1);
+            Console.WriteLine("정수2 : " + num2);
+            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+            Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+            Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+            Console.WriteLine(num1 + " / " + num2 + " = " + (num1 / num2));
+            Console.WriteLine(num1 + " % " + num2 + " = " + (num1 % num2));
+        }
+    }
+}
